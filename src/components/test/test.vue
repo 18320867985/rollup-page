a82351130d07f8c9536c255341339cfba1258be1
18320867985 <724485868@qq.com>
2021-07-13 22:20:01 +0800

    2021-7-13

---
+++ src/components/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="pp">{{test}}
+	<hr>
+	<input type="text" name="" id="" value="" v-model="test" />
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				test:"vue-dev hqs 6688"
+			}
+		}
+	}
+</script>
+
+<style scoped="">
+	.pp{
+		font-size: 30px;
+		color:red;
+	}
+</style>
---
+++ src/js-dev/all.js
@@ -0,0 +1,7 @@
+
+// import "";
+// export {} from "";
+
+import "../common/autoRun";
+export { index } from "./pages/index";
+export { test } from "./pages/test";
